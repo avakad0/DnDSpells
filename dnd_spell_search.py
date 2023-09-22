b1b6bc5d9a8ab0c98c376846dd53c1873cbedc83
avakad0 <36058984+avakad0@users.noreply.github.com>
2023-09-22 14:54:02 -0700

    Changed GUI to customTKinter and added live search function
    -Changed GUI tkinter -> customTKinter
    -Added 'live-search'
    -Added press enter for search
    -Added new frame to display the spell after clicked

--- dnd_spell_search.py
+++ dnd_spell_search.py
@@ -1,7 +1,6 @@
 import requests
 import json
-import tkinter as tk
-from tkinter import ttk
+import customtkinter as ctk
 
 # Function to fetch spell data from the D&D 5e API
 def fetch_spells(spell_name):
@@ -13,36 +12,84 @@ def fetch_spells(spell_name):
     else:
         print(f"Failed to get data: {response.status_code}")
         return None
+    
+spell_details = {}
 
 # Function to search for a spell using the GUI
 def search_spell():
+    global spell_details
     spell_name = spell_name_entry.get()
-    spell_listbox.delete(0, tk.END)  # Clear previous search results
+    spell_data = fetch_spells(spell_name)  
+    print(spell_data)  # Add the print statement here
+    # Clear previous search results
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
+    
     spell_data = fetch_spells(spell_name)
     
     if spell_data and 'results' in spell_data:
         for spell in spell_data['results']:
-            spell_listbox.insert(tk.END, spell['name'])
+            label = ctk.CTkLabel(scrollable_frame, text=spell['name'], cursor="hand2")
+            label.pack(pady=5)
+            label.bind("<Button-1>", lambda e, s=spell: display_spell_details(s))
+            spell_details[spell['name']] = spell
     else:
-        spell_listbox.insert(tk.END, "No spell found.")
+        label = ctk.CTkLabel(scrollable_frame, text="No spell found.")
+        label.pack(pady=5)
 
-# Tkinter GUI setup
-app = tk.Tk()
+def display_spell_details(spell):
+    # Clear existing details
+    for widget in details_frame.winfo_children():
+        widget.destroy()
+
+    # Spell name
+    name_label = ctk.CTkLabel(details_frame, text=spell['name'], font=("Arial", 18))
+    name_label.pack(pady=10)
+
+    # Spell description
+    description = spell.get('desc', ["Description not available."])[0]  # Default to "Description not available."
+    desc_label = ctk.CTkLabel(details_frame, text=spell['desc'][0])  # Displaying the first line of description
+    desc_label.pack(pady=10)
+
+# Function to handle Enter key press
+def on_enter_key(event):
+    search_spell()
+
+search_after_id = None
+
+def delayed_search():
+    global search_after_id
+
+    # If there's an existing scheduled search, cancel it
+    if search_after_id:
+        spell_name_entry.after_cancel(search_after_id)
+
+    # Schedule the new search
+    search_after_id = spell_name_entry.after(300, search_spell)
+
+# customTkinter GUI setup
+app = ctk.CTk()
 app.title("D&D 5e Spell Search")
+app.geometry("600x400")
+app.resizable(True, True)
 
-frame = ttk.Frame(app, padding="10")
-frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+frame = ctk.CTkFrame(app)
+frame.place(relx=0.5, rely=0.5, anchor="center", relwidth=0.9, relheight=0.9)
 
-spell_name_entry = ttk.Entry(frame, width=30)
-spell_name_entry.grid(row=0, column=0, sticky=(tk.W, tk.E))
+spell_name_entry = ctk.CTkEntry(frame, width=400)
+spell_name_entry.grid(row=0, column=0, sticky="w", pady=20)
+spell_name_entry.bind("<Return>", on_enter_key)  # Bind Enter key
+spell_name_entry.bind("<KeyRelease>", lambda event: delayed_search())  # Bind to KeyRelease
 
-search_button = ttk.Button(frame, text="Search", command=search_spell)
-search_button.grid(row=0, column=1)
+search_button = ctk.CTkButton(frame, text="Search", command=search_spell)
+search_button.grid(row=0, column=1, pady=20)
 
-spell_listbox = tk.Listbox(frame, width=50, height=15)
-spell_listbox.grid(row=1, columnspan=2, sticky=(tk.W, tk.E))
+# Using CTkScrollableFrame for results
+scrollable_frame = ctk.CTkScrollableFrame(frame)
+scrollable_frame.grid(row=1, columnspan=2, pady=20, sticky="w")
 
-frame.columnconfigure(0, weight=1)
-frame.rowconfigure(1, weight=1)
+# Create a frame for displaying spell details
+details_frame = ctk.CTkFrame(app)
+details_frame.place(relx=0.6, rely=0.5, anchor="center", relwidth=0.35, relheight=0.5)
 
 app.mainloop()
