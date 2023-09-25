3ec64ef21298235dc25d5a0059dcab275ac0dcca
avakad0 <36058984+avakad0@users.noreply.github.com>
2023-09-24 19:41:04 -0700

    SO MANY THINGS
    -made the window smaller
    -LIVE SEARCH!
    -fixed desc window
    -removed search button
    -reordered desc

--- dnd_spell_search.py
+++ dnd_spell_search.py
@@ -1,76 +1,197 @@
+import sys
+sys.path.append("/CTKScrollableDropdown/")
+
 import requests
 import json
+from tkinter import *
 import customtkinter as ctk
+from CTkScrollableDropdown import *
+from cachetools import TTLCache
+
+#
+# GUI SET UP
+#
+#
+
+app = ctk.CTk()
+app.title("D&D 5e Spell Search")
+app.geometry("1000x800")
+
+cache = TTLCache(maxsize=100, ttl=3600)
 
 BASE_URL = "http://www.dnd5eapi.co"
 
-# Function to fetch spell data from the D&D 5e API
-def fetch_spells(spell_name=None, url=None):
-    if url:
-        # Prepend the BASE_URL to the provided relative URL
-        response = requests.get(BASE_URL + url)
-    else:
-        response = requests.get(f"{BASE_URL}/api/spells?name={spell_name}")
+search_after_id = None
 
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return data
-    else:
-        print(f"Failed to get data: {response.status_code}")
+#
+# API-Related Functions
+#
+#
+#
+
+def fetch_spells(spell_name=None, url=None):
+    try:
+        # Check if the response is in the cache
+        cache_key = url if url else f"{BASE_URL}/api/spells?name={spell_name}"
+        cached_response = cache.get(cache_key)
+        if cached_response:
+            return cached_response
+
+        target_url = BASE_URL + url if url else f"{BASE_URL}/api/spells?name={spell_name}"
+        response = requests.get(target_url)
+
+        print("Request URL:", target_url)  # Print the request URL for debugging
+
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx HTTP status codes
+
+        if response.status_code == 200:
+            data = response.json()
+
+            # Cache the response for future use
+            cache[cache_key] = data
+
+            return data
+        else:
+            print(f"Failed to get data: {response.status_code}")
+            print("Response Content:", response.content)  # Print the response content for debugging
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return None
 
+def get_spell_names():
+    """
+    Fetch all spells names to populate the dropdown.
+    """
+    response = requests.get(f"{BASE_URL}/api/spells")
+    if response.status_code == 200:
+        data = response.json()
+        if "results" in data:
+            # Return a list of dictionaries containing name and URL
+            return [{"name": spell['name'], "url": spell['url']} for spell in data['results']]
+    return []
 
-    
-spell_details = {}
 
-# Function to search for a spells using the GUI
-def search_spell():
-    global spell_details
 
-    spell_name = spell_name_entry.get() 
-    spell_data = fetch_spells(spell_name)
+#
+# Live search function
+#
+#
 
-    # Clear existing spell names from the scrollable frame
-    for widget in scrollable_frame.winfo_children():
-        widget.destroy()
+spell_data = {spell['name']: spell['url'] for spell in get_spell_names()}
 
-    if spell_data and 'results' in spell_data:
-        for spell in spell_data['results']:
-            label = ctk.CTkLabel(scrollable_frame, text=spell['name'])
-            label.bind("<Button-1>", lambda e, s=spell: display_spell_details(s))
-            label.pack(fill="x", pady=2)
-    else:
-        label = ctk.CTkLabel(scrollable_frame, text="No spell found.")
-        label.pack(pady=10)
+entry = ctk.CTkEntry (app, width=400, placeholder_text="Search")
+entry.pack(padx=10, pady=10)
 
-def display_spell_details(spell):
-    # Fetch full details of the spell
+def on_spell_selected(spell_name):
+    spell_url = spell_data.get(spell_name)
+    if spell_url:
+        entry.delete(0, 'end')
+        entry.insert(0, spell_name)
+        display_spell_details({"name": spell_name, "url": spell_url}, details_frame)
+
+CTkScrollableDropdown(entry, values=list(spell_data.keys()), command=on_spell_selected, autocomplete=True)
+
+def delayed_search(event=None):
+    """
+    Delay search results
+    """
+    global search_after_id
+
+    # If there's an existing scheduled search, cancel it
+    if search_after_id:
+        app.after_cancel(search_after_id)
+
+    # Schedule the update_dropdown function after a short delay (e.g., 500ms)
+    search_after_id = app.after(500, update_dropdown)
+
+    def update_dropdown():
+        current_input = entry.get().lower()
+        updated_values = [spell for spell in get_spell_names() if current_input in spell.lower()]
+
+    entry.bind("<KeyRelease>", delayed_search)
+
+#
+# Display details of the selected spell
+#
+#
+
+def display_spell_details(spell, details_frame):
+    # Fetch complete spell details
     full_spell_data = fetch_spells(url=spell['url'])
+
     # PRINT
-    print(full_spell_data)  
+    #print(full_spell_data)  
 
     # Clear existing details
     for widget in details_frame.winfo_children():
         widget.destroy()
 
+    # Classes
+    classes_list = full_spell_data.get('classes', [])
+    class_names = ', '.join([cls['name'] for cls in classes_list])
+    classes_label = ctk.CTkLabel(details_frame, text=f"Classes: {class_names}")
+    classes_label.pack(pady=5)
+
     # Spell name
     name_label = ctk.CTkLabel(details_frame, text=spell['name'], font=("Arial", 18))
-    name_label.pack(pady=10)
+    name_label.pack(pady=0)
+
+    separator_width = 200
+    separator = Frame(details_frame, width=separator_width, height=2, bg="grey")
+    separator.pack(padx=5, pady=0)
+
+    # Level School of magic
+    level = full_spell_data.get('level', 0)
+    level_display = "Cantrip" if level == 0 else f"Level {level}"
+    school = full_spell_data.get('school', {}).get('name', 'Unknown School')
+    school_label = ctk.CTkLabel(details_frame, text=f"{level_display} {school} Spell")
+    school_label.pack(pady=0)
 
     # Casting Time and Level/Cantrip
     casting_time = full_spell_data.get('casting_time', 'Unknown Casting Time')
-    level = full_spell_data.get('level', 0)
-    level_display = "Cantrip" if level == 0 else f"Level {level}"
     concentration_text = f"Concentration: {'Yes' if full_spell_data.get('concentration', False) else 'No'}"
-    casting_level_label = ctk.CTkLabel(details_frame, text=f"{level_display} | Casting Time: {casting_time} | {concentration_text}")
-    casting_level_label.pack(pady=10)
+    casting_time_label = ctk.CTkLabel(details_frame, text=f"Casting Time: {casting_time} | {concentration_text}")
+    casting_time_label.pack(pady=10)
 
-    # Damage
-    damage_info = full_spell_data.get('damage', {}).get('damage_type', {}).get('name')
-    if damage_info:
-        damage_text = f"Damage: {damage_info}"
+    def calculate_damage_range(damage_formula):
+        """
+        Calculates the min and max damage from a given damage formula (e.g., '2d6').
+        Returns a tuple (min_damage, max_damage).
+        """
+        try:
+            num_dice, dice_type = map(int, damage_formula.split('d'))
+            return (num_dice, num_dice * dice_type)
+        except ValueError:
+            return (None, None)
+
+    # Fetch damage data
+    damage_data = full_spell_data.get('damage', {})
+    damage_at_slot_level = damage_data.get('damage_at_slot_level', {})
+    damage_type = damage_data.get('damage_type', {}).get('name', "Unknown Type")
+
+    # Identify the lowest spell slot level
+    lowest_slot_level = min(map(int, damage_at_slot_level.keys())) if damage_at_slot_level else None
+
+    if lowest_slot_level:
+        damage_formula = damage_at_slot_level[str(lowest_slot_level)]
+        min_damage, max_damage = calculate_damage_range(damage_formula)
+        damage_text_min_max = f"{min_damage} - {max_damage} Damage"
+        damage_text_formula = f"{damage_formula} {damage_type}"
     else:
-        damage_text = ""
+        damage_text_min_max = ""
+        damage_text_formula = ""
+
+    if damage_text_min_max:
+        damage_label_min_max = ctk.CTkLabel(details_frame, text=damage_text_min_max)
+        damage_label_min_max.pack(pady=5)  # You can adjust padding as necessary
+
+    if damage_text_formula:
+        damage_label_formula = ctk.CTkLabel(details_frame, text=damage_text_formula)
+        damage_label_formula.pack(pady=5)  # You can adjust padding as necessary
 
     # Area of Effect
     area_of_effect_type = full_spell_data.get('area_of_effect', {}).get('type')
@@ -82,12 +203,20 @@ def display_spell_details(spell):
     else:
         area_of_effect_text = ""
 
-    # Combine Damage and Area of Effect and display them
-    details_texts = [text for text in [damage_text, area_of_effect_text] if text]  # filter out empty strings
-    combined_text = " | ".join(details_texts)
-    if combined_text:  # Only pack the label if there's content
-        combined_label = ctk.CTkLabel(details_frame, text=combined_text)
-        combined_label.pack(pady=10)
+    # Area of Effect
+    area_of_effect_type = full_spell_data.get('area_of_effect', {}).get('type')
+    area_of_effect_size = full_spell_data.get('area_of_effect', {}).get('size')
+    if area_of_effect_type and area_of_effect_size:
+        area_of_effect_text = f"Area of Effect: {area_of_effect_type} ({area_of_effect_size})"
+    elif area_of_effect_type:
+        area_of_effect_text = f"Area of Effect: {area_of_effect_type}"
+    else:
+        area_of_effect_text = ""
+
+    # Show Area of Effect label
+    if area_of_effect_text:  # Only pack the label if there's content
+        area_of_effect_label = ctk.CTkLabel(details_frame, text=area_of_effect_text)
+        area_of_effect_label.pack(pady=10)
 
     # Displaying the range, components, material, duration, and concentration
     range_text = f"Range: {full_spell_data.get('range', 'N/A')}"
@@ -98,11 +227,6 @@ def display_spell_details(spell):
     details_label = ctk.CTkLabel(details_frame, text=details_text, wraplength=450)  # Adjust 450 to your frame's width
     details_label.pack(pady=10)
 
-    # School of magic
-    school = full_spell_data.get('school', {}).get('name', 'Unknown School')
-    school_label = ctk.CTkLabel(details_frame, text=f"School: {school}")
-    school_label.pack(pady=10)
-
     # Spell description
     description = "\n\n".join(full_spell_data.get('desc', ["Description not available."]))
 
@@ -110,51 +234,17 @@ def display_spell_details(spell):
     higher_level_desc = full_spell_data.get('higher_level')
     if higher_level_desc:
         description += "\n\n" + "\n\n".join(higher_level_desc)
-
-    desc_label = ctk.CTkLabel(details_frame, text=full_spell_data.get('desc', ["Description not available."])[0])
+        
     desc_label = ctk.CTkLabel(details_frame, text=description, wraplength=400)
-
     desc_label.pack(pady=10)
+#
+# GUI Configuration
+#
+#
+#
 
-# Function to handle Enter key press
-def on_enter_key(event):
-    search_spell()
-
-search_after_id = None
-
-def delayed_search():
-    global search_after_id
-
-    # If there's an existing scheduled search, cancel it
-    if search_after_id:
-        spell_name_entry.after_cancel(search_after_id)
-
-    # Schedule the new search .after(ms)
-    search_after_id = spell_name_entry.after(200, search_spell)
-
-
-# customTkinter GUI setup
-app = ctk.CTk()
-app.title("D&D 5e Spell Search")
-app.geometry("1200x800")
-app.resizable(True, True)
-
-frame = ctk.CTkFrame(app)
-frame.place(relx=0.5, rely=0.5, anchor="center", relwidth=0.9, relheight=0.9)
-
-spell_name_entry = ctk.CTkEntry(frame)
-spell_name_entry.grid(row=0, column=0, sticky="nsew")
-spell_name_entry.bind("<Return>", on_enter_key)
-spell_name_entry.bind("<KeyRelease>", lambda event: delayed_search())
-
-search_button = ctk.CTkButton(frame, text="Search", command=search_spell)
-search_button.grid(row=0, column=1, padx=(10, 50))
-
-scrollable_frame = ctk.CTkScrollableFrame(frame)
-scrollable_frame.grid(row=1, columnspan=2, pady=20, sticky="nsew")
-
-# Adjust the relative width and height
+# Details frame
 details_frame = ctk.CTkScrollableFrame(app)
-details_frame.place(relx=0.6, rely=0.5, anchor="center", relwidth=0.4, relheight=0.6)
+details_frame.pack(side = LEFT, fill = BOTH, expand= TRUE, padx=20, pady=20)
 
 app.mainloop()
